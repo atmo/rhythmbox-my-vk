8581e2df166d89ca30a807c94e9c85acff7ff46a
Stanislav Rogovskiy <stanislav.rogovskiy@gmail.com>
2015-03-11 16:20:22 +0200

    Implemented source window with icon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-rhythmbox-vk
+rhythmbox-my-vk
 ============
 
 rhythmbox-3 plugin for vk.com
--- myvk.py
+++ myvk.py
@@ -1,4 +1,4 @@
-from gi.repository import GObject, RB, Peas
+from gi.repository import GObject, RB, Peas, Gio
 
 
 class MyVKPlugin (GObject.Object, Peas.Activatable):
@@ -10,5 +10,36 @@ class MyVKPlugin (GObject.Object, Peas.Activatable):
     def do_activate(self):
         print("Plugin activated.")
 
+        shell = self.object
+        db = shell.props.db
+        entry_type = VKEntryType()
+        db.register_entry_type(entry_type)
+        iconfile = Gio.File.new_for_path(
+            self.plugin_info.get_data_dir() + "/logo.svg")
+        self.source = GObject.new(VKSource, shell=shell,
+                                  name=_("VK Music"),
+                                  entry_type=entry_type,
+                                  icon=Gio.FileIcon.new(iconfile))
+        shell.register_entry_type_for_source(self.source, entry_type)
+
+        group = RB.DisplayPageGroup.get_by_id("library")
+        shell.append_display_page(self.source, group)
+
     def do_deactivate(self):
-        print("Deactivating...")
+        print("Pluging deactivated.")
+
+
+class VKEntryType(RB.RhythmDBEntryType):
+
+    def __init__(self):
+        RB.RhythmDBEntryType.__init__(self, name='vk-entry-type')
+
+
+class VKSource(RB.BrowserSource):
+
+    def __init__(self):
+        super(VKSource, self).__init__()
+        RB.BrowserSource.__init__(self)
+
+
+GObject.type_register(VKSource)
